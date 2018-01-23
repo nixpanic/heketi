0556e42ecf510c9d3bd382aff31fa458ab27b095
Niels de Vos <ndevos@redhat.com>
2018-01-23 19:16:39 +0100

    [WIP] heketi: add support for profiling with pprof
    Add support for profiling like "net/http/pprof" does, but through the
    "gorilla/mux" router. Profiling is disabled by default, set the
    `HEKETI_PROFILING` environment variable to a non-empty string or add
    `"profiling": true` to the --config file.
    
    Several endpoints will get registered, and are writter to stderr:
    
        $ HEKETI_PROFILING=1 ./heketi --config=heketi.json
        ...
        DEBUG: registered profiling handler on /debug/pprof/block
        DEBUG: registered profiling handler on /debug/pprof/goroutine
        DEBUG: registered profiling handler on /debug/pprof/heap
        DEBUG: registered profiling handler on /debug/pprof/mutex
        DEBUG: registered profiling handler on /debug/pprof/threadcreate
        DEBUG: registered profiling handler on /debug/pprof/cmdline
        DEBUG: registered profiling handler on /debug/pprof/profile
        DEBUG: registered profiling handler on /debug/pprof/symbol
        DEBUG: registered profiling handler on /debug/pprof/trace
        ...
    
    Standard tools like `go tool pprof` and `curl` work. Examples:
    
        $ go tool pprof http://localhost:8080/debug/pprof/profile
        $ curl http://localhost:8080/debug/pprof/heap?debug=1
    
    URL: https://golang.org/pkg/net/http/pprof/
    Updates: #799
    Signed-off-by: Niels de Vos <ndevos@redhat.com>

---
+++ pkg/utils/profiling.go
@@ -0,0 +1,48 @@
+//
+// Copyright (c) 2018 The heketi Authors
+//
+// This file is licensed to you under your choice of the GNU Lesser
+// General Public License, version 3 or any later version (LGPLv3 or
+// later), or the GNU General Public License, version 2 (GPLv2), in all
+// cases as published by the Free Software Foundation.
+
+package utils
+
+// Based on ideas from https://github.com/mistifyio/negroni-pprof
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/http/pprof"
+	runtime_pprof "runtime/pprof"
+
+	"fmt"
+	"os"
+)
+
+var (
+	basePath string = "/debug/pprof/"
+)
+
+func addPath(router *mux.Router, name string, handler http.Handler) {
+	router.Path(basePath + name).Name(name).Handler(handler)
+	fmt.Fprintf(os.Stderr, "DEBUG: registered profiling handler on %s\n", basePath + name)
+}
+
+func EnableProfiling(router *mux.Router) {
+	for _, profile := range runtime_pprof.Profiles() {
+		name := profile.Name()
+		handler := pprof.Handler(name)
+
+		addPath(router, name, handler)
+	}
+
+	// static profiles as listed in net/http/pprof/pprof.go:init()
+	addPath(router, "cmdline", http.HandlerFunc(pprof.Cmdline))
+	addPath(router, "profile", http.HandlerFunc(pprof.Profile))
+	addPath(router, "symbol", http.HandlerFunc(pprof.Symbol))
+	addPath(router, "trace", http.HandlerFunc(pprof.Trace))
+
+	// The index handler only lists the runtime_pprof.Profiles(), so let's not index any.
+	//router.Methods("GET").Path(basePath).Name("index").Handler(http.HandlerFunc(pprof.Index))
+}
